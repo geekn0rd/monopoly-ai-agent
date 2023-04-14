2cf301a0a8ccc84724e8fa2c179f87e5c90f51e5
Amir Khani <99316850+IAmirKhani@users.noreply.github.com>
2023-04-14 10:19:02 +0330

    Modifying some of functions

--- player.py
+++ player.py
@@ -1,32 +1,39 @@
 import random
 
-# Define the Player class
-class Player:
-    def __init__(self, name: str, position: int, money: int):
+class Player():
+    def __init__(self, name, position, money):
         self.name = name
         self.position = position
         self.money = money
+        self.is_in_jail = False
         self.properties = []
-        self.in_jail = False
-        self.turns_in_jail = 0
-        self.rolled_doubles = False
     
-    def pay(self, amount) -> None:
+    def pay_money(self, amount):
         self.money -= amount
 
-    def receive(self, amount) -> None:
+    def get_money(self, amount):
         self.money += amount
-    
-    def roll_dice(self) -> tuple:
-        # Roll the dice and return the result
+
+    def roll_dice(self):
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
-        self.rolled_doubles = True if dice1 == dice2 else False
-        return (dice1, dice2)
+        total = dice1 + dice2
+        print(f"{self.name} rolled {dice1} and {dice2} ({total})")
+        return total
     
-    def net_worth(self, props) -> int:
-        return self.money + sum([props[i].price for i in self.properties]) + sum([props[i].rent for i in self.properties])
     
-    def __str__(self) -> str:
-        return f"{self.name} (Position: {self.position}, Money: {self.money}$, Properties: {self.properties})"
+    def net_worth(self, props):
+        # Calculate the player's total net worth by adding their money and the total value of their properties
+        total_worth = self.money + sum([prop.price for prop in self.properties])
+        
+        # Add the total rent earned from all the player's properties
+        total_rent = sum([prop.rent for prop in self.properties])
+        
+        return total_worth + total_rent
 
+    def __str__(self):
+        # Convert the list of properties to a comma-separated string of property names
+        prop_names = ", ".join([prop.name for prop in self.properties])
+        
+        # Format the string using f-strings and return it
+        return f"{self.name} (Position: {self.position}, Money: {self.money}$, Properties: {prop_names})"
