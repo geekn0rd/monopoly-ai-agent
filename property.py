7cb39f88c83650aa3b59897710af1bdef8cbcc58
Amir Khani <99316850+IAmirKhani@users.noreply.github.com>
2023-04-14 10:20:24 +0330

    simplifying the Property class and changing the __str__ func

--- property.py
+++ property.py
@@ -1,19 +1,13 @@
-# Define the Property class
-class Property:
-    def __init__(self, name: str, space: str, color: str, position: int, price: int, rent: int, build_price: int):
+
+class Property():
+    def __init__(self, name, space, position, price, rent, build_price) -> None:
         self.name = name
         self.space = space
-        self.color = color
         self.position = position
         self.price = price
         self.rent = rent
         self.build_price = build_price
         self.ownable = False
-        if space in ["Street", "Railroad", "Utility"]:
-            self.ownable = True
-            self.owner = None
-            self.level = 1
-    
-    def __str__(self) -> str:
-        return f"{self.name} (Price: {self.price}, Rent: {self.rent})"
-
+    def __str__(self):
+        # Format the string using f-strings and return it
+        return f"{self.name} - Price: {self.price}$, Rent: {self.rent}$"
