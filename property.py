d6ec5187f45885d7491d28826237f667314f8c53
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-04-14 12:55:54 +0330

    added upgrade method

--- property.py
+++ property.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 # Define the Property class
 class Property:
     def __init__(self, name: str, space: str, color: str, position: int, price: int, rent: int, build_price: int):
@@ -13,6 +15,11 @@ class Property:
             self.ownable = True
             self.owner = None
             self.level = 1
+    
+    def upgrade(self) -> None:
+        self.rent *= 1.5
+        self.rent = ceil(self.rent)
+        self.level += 1
     
     def __str__(self) -> str:
         return f"{self.name} (Price: {self.price}, Rent: {self.rent})"
